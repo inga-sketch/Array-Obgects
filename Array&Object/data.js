b65550b0840840a189317ce6b843e9abd58a2f8e
inga-sketch <ingacihicina@gmail.com>
2025-04-25 12:00:57 +0300

    Add files via upload

---
+++ Array&Object/data.js
@@ -0,0 +1,20 @@
+let reviews = [
+    {
+        body: 'I liked it very much!',
+        author:"John Doe",
+        rating: 3.5,
+        real: true
+    },
+    {
+        body: 'I  will definitely invite my friends there!',
+        author:"Marry P.",
+        rating: 4.5,
+        real: true
+    },
+    {
+        body: 'Bad food... x(',
+        author:"Pete K.",
+        rating: 1.5,
+        real: false
+    }
+]
